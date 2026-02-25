f647f20c6949f1a22bf857a4d5dffa74a37b7244
gogom0nster <2002kitovoty@gmail.com>
2026-02-25 21:23:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ document.addEventListener("DOMContentLoaded", function () {
     "./images/img3.png",
     "./images/img4.png",
     "./images/img5.png"
+    "./images/img6.png",
+    "./images/img7.png",
+    "./images/img8.png",
+    "./images/img9.png",
+    "./images/img10.png"
   ];
 
   for (let i = 0; i < 9; i++) {
