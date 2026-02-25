762401b1c203ac0eedda62b5d4abbb1200b51044
gogom0nster <2002kitovoty@gmail.com>
2026-02-25 18:03:05 +0900

    Update Firebase config and image URLs in script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase import
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAcgpImd4xQQrXjsdeUtISiI0blO6qHNhQ",
-    authDomain: "hometown-data.firebaseapp.com",
-    projectId: "hometown-data",
-    storageBucket: "hometown-data.firebasestorage.app",
-    messagingSenderId: "427121820704",
-    appId: "1:427121820704:web:ae205795fbcac3e713e845",
-    measurementId: "G-3WQWD0BW3F"
-  };
+// Firebase config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "hometown-data.firebaseapp.com",
+  projectId: "hometown-data",
+  storageBucket: "hometown-data.firebasestorage.app",
+  messagingSenderId: "427121820704",
+  appId: "1:427121820704:web:ae205795fbcac3e713e845",
+  measurementId: "G-3WQWD0BW3F"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// -----------------
+// GRID 생성 코드
+// -----------------
+
 const grid = document.getElementById("grid");
 
 grid.style.display = "grid";
@@ -32,7 +32,7 @@ for (let i = 0; i < 9; i++) {
   const cell = document.createElement("div");
 
   const isImageA = Math.random() > 0.5;
-  const imageUrl = isImageA ? "images/a.jpg" : "images/b.jpg";
+  const imageUrl = isImageA ? "images/img1.png" : "images/img2.png";
 
   cell.style.width = "150px";
   cell.style.height = "150px";
